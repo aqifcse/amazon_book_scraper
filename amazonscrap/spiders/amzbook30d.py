c4c02116f3177be92fb33d29943c709c53837667
Syed Towfiqur Rahim <5446400-aqifcse@users.noreply.gitlab.com>
2021-02-11 18:12:20 +0600

    Final Code

--- amazonscrap/spiders/amzbook30d.py
+++ amazonscrap/spiders/amzbook30d.py
@@ -1,3 +1,4 @@
+# page-by-page
 # -*- coding: utf-8 -*-
 import scrapy
 
@@ -38,4 +39,47 @@ class Amzbook30dSpider(scrapy.Spider):
 
 
 
+# book-by-book
+# import scrapy
+
+# class Amzbook30dSpider(scrapy.Spider):
+#     name = 'amzbook30d'
+#     allowed_domains = ['amazon.com']
+#     start_urls = ['https://www.amazon.com/Books-Last-30-days/s?rh=n%3A283155%2Cp_n_publication_date%3A1250226011/']     # '/' must me included :(
+#     base_url = 'https://www.amazon.com/Books-Last-30-days/s?rh=n%3A283155%2Cp_n_publication_date%3A1250226011/'         # '/' must be included  :(
+    
+#     def parse(self, response):
+#         all_books = response.xpath('//div[@class="s-include-content-margin s-border-bottom s-latency-cf-section"]')
+
+#         for book in all_books:
+#             book_url = book.xpath('.//h2/a/@href').extract_first()
+#             yield scrapy.Request(url = response.urljoin(book_url), callback=self.parse_book)
+        
+
+#         next_page_partial_url = response.xpath('//li[@class="a-last"]/a/@href').extract_first()
+#         if next_page_partial_url:
+#             next_page_url = response.urljoin(next_page_partial_url)
+
+#             yield scrapy.Request(url = next_page_url, callback = self.parse)
+
+#         else: print("I am Done!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    
+        
+#     def parse_book(self, response):
+
+#         title               = response.xpath('//h1[@class="a-spacing-none a-text-normal"]/span/text()').extract_first()
+#         author              = response.xpath('//a[@class="a-link-normal contributorNameID"]/text()').extract_first()
+#         published_date      = response.xpath('//span[@class="a-size-large a-color-secondary"]/text()').extract_first()
+#         rating              = response.xpath('//span[@class="acrCustomerReviewText"]/text()').extract_first()
+#         hardcover_price_int = response.xpath('//span[@class="a-size-base a-color-price a-color-price"]/text()').extract_first()
+#         audible_status      = response.xpath('//span[@class="a-size-medium a-color-success"]/text()').extract_first()
+
+#         yield {
+#             'title' :           title, 
+#             'author' :          author, 
+#             'published_date':   published_date, 
+#             'rating' :          rating, 
+#             'price' :           hardcover_price_int, 
+#             'status' :          audible_status, 
+#         }
         
